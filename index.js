cdbc20f606460dadce378bbc692f60379131b43f
aprendiendoEmma <emma97perez97@gmail.com>
2021-03-06 18:25:27 -0300

    se creo el servidor para poder practicar

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const Persona = require('./clases/Persona');
 const app = express();
-
 app.use(express.static(__dirname + '/public/'));
 
+//ACA VAN LAS IMPORTACIONES
+const Persona = require('./clases/Persona');
+const nuevoObjeto = require('./otraCarpeta/nuevoObjeto');
+
+
+//ACA VA EL CODIGO QUE SE VA A EJECUTAR SIEMPRE!
 app.listen('3000', function() {
-    const per = new Persona('emma', 23);
-    console.log(per);
+
 });
