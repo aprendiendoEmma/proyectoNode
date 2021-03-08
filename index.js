8243ac57be7fdf7ea327693c311d4981b11f338d
aprendiendoEmma <emma97perez97@gmail.com>
2021-03-08 13:19:46 -0300

    se modifico el archivo index.js

--- index.js
+++ index.js
@@ -6,38 +6,48 @@ app.use(express.static(__dirname + '/public/'));
 const Estudiante = require('./clases/Estudiante');
 const Docente = require('./clases/Docente');
 const Comision = require('./clases/Comision');
-const Persona = require('./clases/persona');
-
+const Ungs = require('./clases/Ungs');
 //ACA VA EL CODIGO QUE SE VA A EJECUTAR SIEMPRE!
 app.listen('3000', function() {
-    // const estudiantes = crearEstudiantes()
-    // const califaciones = [9, 7, 5, 4];
-    // const docentes = crearDocentes();
-    // const comision1 = new Comision('programacion1', 1, docentes, estudiantes, califaciones);
-    // console.log(comision1);
-    
+    const estudiantes = crearEstudiantes()
+    const califaciones = [9, 7, 5, 4];
+    const docentes = crearDocentes();
+    const comision1 = new Comision('programacion1', 1, docentes, estudiantes, califaciones);
+    console.log(comision1);
+
+
+    cantidadDeDivisores(20)
 
 });
 
-// const crearEstudiantes = () => {
+const crearEstudiantes = () => {
 
-//     const estudiante1 = new Estudiante('marcos', 123);
-//     const estudiante2 = new Estudiante('emma', 345);
-//     const estudiante3= new Estudiante('cristian', 567);
-//     const estudiante4 = new Estudiante('guille', 789);
+    const estudiante1 = new Estudiante('marcos', 123);
+    const estudiante2 = new Estudiante('emma', 345);
+    const estudiante3= new Estudiante('cristian', 567);
+    const estudiante4 = new Estudiante('guille', 789);
 
-//     return [estudiante1,estudiante2,estudiante3,estudiante4];
-// }
+    return [estudiante1,estudiante2,estudiante3,estudiante4];
+}
 
-// const crearDocentes = () => {
+const crearDocentes = () => {
 
-//     const docente1 = new Docente('docente1', 1234567);
-//     const docente2 = new Docente('docente2', 1234568);
-//     const docente3 = new Docente('docente3', 1234569);
-//     const docente4 = new Docente('docente4', 1234560);
+    const docente1 = new Docente('docente1', 1234567);
+    const docente2 = new Docente('docente2', 1234568);
+    const docente3 = new Docente('docente3', 1234569);
+    const docente4 = new Docente('docente4', 1234560);
 
-//     return [docente1, docente2, docente3, docente4];
-// }
+    return [docente1, docente2, docente3, docente4];
+}
 
 
-
+const cantidadDeDivisores = (numero) => {
+    let divisores = 0
+    for (let i = 0; i < numero; i++) {
+        if (numero%i=== 0) {
+            divisores++
+            console.log(i)
+        }
+    }
+    console.log(divisores)
+}
