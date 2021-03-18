66b300d2eb9c07211525fa977485b740d3d3bc80
emma perez <emma97perez97@gmail.com>
2021-03-18 16:35:51 -0300

    modificaron las clases del eejercicio 11

---
+++ clases/ejercicio11/avion.js
@@ -0,0 +1,9 @@
+'use strict';
+
+
+module.exports = class Avion {
+    constructor(tipo , capacidad){
+        this.tipo = tipo;
+        this.capacidad = capacidad;
+    }
+}
---
+++ clases/ejercicio11/vuelo.js
@@ -0,0 +1,9 @@
+'use strict';
+
+
+module.exports = class Vuelo {
+    constructor(avion) {
+        this.avion = avion;
+        this.tripulantes = [];
+    }
+}
