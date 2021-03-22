6a1a31a17c0f667e03566871c5af0e819b6228c1
aprendiendoemma <emma97perez97@gmail.com>
2021-03-22 15:40:02 -0300

    se crearon archivos nuevos

---
+++ indexCursoNode.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname + '/public/'));
+//ACA VAN LAS IMPORTACIONES
+
+
+
+//ACA VA EL CODIGO QUE SE VA A EJECUTAR SIEMPRE!
+app.listen('3000', function() {
+
+
+
+});
